cc33b993366e72a62e9ecd0e2af60c2523eac1ff
yss <today3504@gmail.com>
2024-11-10 17:14:06 +0900

    deploy test4

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         <code>deployTest</code>
+         <code>deployTest2</code>
         </p>
         <a
           className="App-link"

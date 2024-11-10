a4160478143524b348aa4d467d40e34d91f33819
yss <today3504@gmail.com>
2024-11-10 22:24:41 +0900

    site transition test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignInComponent from './app/component/sign-in/sign-in.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <code>싫어</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignInComponent />
     </div>
   );
 }

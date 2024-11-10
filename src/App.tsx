fb215ca028342d12dc300e8b73daec1ab90bcfa5
yss <today3504@gmail.com>
2024-11-10 17:47:36 +0900

    deploy test7

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  const title = 'My React App';
-
+function App() {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>Welcome to my React application!</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <code>싫어</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;

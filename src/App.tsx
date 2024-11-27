4973c5221b2e60efc3d1d6585fb27bd5ecedf9e6
yss <today3504@gmail.com>
2024-11-27 17:07:01 +0900

    rollback

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,15 @@ const App: React.FC = () => {
 
     return (
         <Router>
+            <div className="App">
+                {authenticated && (
+                    <Header
+                        onLogout={() => {
+                            logout();
+                            window.location.href = "/signin"; // 로그아웃 시 로그인 화면으로 리다이렉트
+                        }}
+                    />
+                )}
                 <Routes>
                     <Route
                         path="/"
@@ -33,6 +42,7 @@ const App: React.FC = () => {
                     />
                     <Route path="/signin" element={<SignInPage />} />
                 </Routes>
+            </div>
         </Router>
     );
 };
